b048305559000be6385af90a257b29ad037771a9
Rokas Kirdulis <rokas.kirdulis@gmail.com>
2024-04-16 23:38:33 +0300

    Code update for tension rig

--- CanSatSoft/Version3.1_Xbee/Build3_1_xbee/Build3_1_xbee.ino
+++ CanSatSoft/Version3.1_Xbee/Build3_1_xbee/Build3_1_xbee.ino
@@ -60,8 +60,11 @@ static const int RXPinGPS = 2, TXPinGPS = 3;
 static const uint32_t GPSBaud = 57600;
 
 
-float targetLat = 54.67578568748265;//54.67836624970636;
-float targetLon = 25.223520410616086;//25.263374017806708;
+float targetLat = 54.67744326669359; //54.67836624970636;
+float targetLon = 25.26362502137699;//25.263374017806708;
+
+float currentLat = 54.67847666414606;
+float currentLon = 25.263313505862428;
 
 float delta_lon, delta_lat;
 float bearing = 0;
@@ -346,8 +349,8 @@ void loop() {
   servoX.write(MapToFloat(sin(radians(GetBearing(targetLat, targetLon, FeedbackData.latitude, FeedbackData.longtitude)) + radians(GetYaw())), 1, -1, 0, 180));
   servoY.write(MapToFloat(cos(radians(GetBearing(targetLat, targetLon, FeedbackData.latitude, FeedbackData.longtitude))  + radians(GetYaw())), 1, -1, 0, 180)); 
   //Serial.print(GetYaw());Serial.print("/");Serial.print(gps.location.lat());Serial.print("/");Serial.print(gps.location.lng());Serial.print("/");Serial.print(GetBearing(targetLat, targetLon, FeedbackData.latitude, FeedbackData.longtitude));Serial.print("/");Serial.println(gps.satellites.value());
-  Serial0.print(GetYaw());Serial0.print("/");Serial0.print(temp_event.temperature);Serial0.print("/");Serial0.print(pressure_event.pressure);Serial0.print("/");Serial0.print(humidity_event.relative_humidity);Serial0.print("/");Serial0.print(MapToFloat(sin(0 + radians(GetYaw())), -1, 1, 0, 180));Serial0.print("/");Serial0.print(GetBearing(targetLat, targetLon, FeedbackData.latitude, FeedbackData.longtitude));//Serial0.print("/");Serial0.println(gps.satellites.value());
-  Serial0.print("/");Serial0.print(gps.location.lat(), 6);Serial0.print("/");Serial0.println(gps.location.lng(), 6);
+  Serial0.print(GetYaw());Serial0.print("/");Serial0.print(temp_event.temperature);Serial0.print("/");Serial0.print(pressure_event.pressure);Serial0.print("/");Serial0.print(humidity_event.relative_humidity);Serial0.print("/");Serial0.print(MapToFloat(sin(0 + radians(GetYaw())), -1, 1, 0, 180));Serial0.print("/");Serial0.print(degrees(GetBearing(targetLat, targetLon, currentLat, currentLon)));//Serial0.print("/");Serial0.println(gps.satellites.value());
+  Serial0.print("/");Serial0.print(gps.location.lat(), 6);Serial0.print("/");Serial0.print(gps.location.lng(), 6);Serial0.print("/X::");Serial0.print(MapToFloat(sin(radians(GetBearing(targetLat, targetLon, currentLat, currentLon)) + radians(GetYaw())), 1, -1, 0, 180));Serial0.print("/Y::");Serial0.println(MapToFloat(cos(radians(GetBearing(targetLat, targetLon, currentLat, currentLon)) + radians(GetYaw())), 1, -1, 0, 180));
   delay(100);
 
   //SetServos(GetYaw());
